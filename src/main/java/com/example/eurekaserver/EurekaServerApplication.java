fbb6661e9fa05fedda3d94804f0f011118134a9b
Neder Mfarrej <76585797+gliv711@users.noreply.github.com>
2023-04-09 22:08:08 +0200

    Initial commit

---
+++ src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.example.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+@CrossOrigin(origins = "http://localhost:4200")
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
